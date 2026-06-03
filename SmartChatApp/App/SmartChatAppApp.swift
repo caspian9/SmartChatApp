3de110b713e3f7ea774e92f4a1690894f555d42c
caspian9 <caspian9@users.noreply.github.com>
2026-06-03 11:13:03 +0800

    feat: add SwiftData container to app

--- SmartChatApp/App/SmartChatAppApp.swift
+++ SmartChatApp/App/SmartChatAppApp.swift
@@ -1,13 +1,25 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct SmartChatAppApp: App {
     @StateObject private var config = ConfigurationManager.shared
 
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([GatewayProfile.self])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
     var body: some Scene {
         WindowGroup {
             RootView()
         }
+        .modelContainer(sharedModelContainer)
     }
 }
 
